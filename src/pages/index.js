e3af8287d7d24193c6fd9aba712194755fb033cd
David Inglody <inglody96@gmail.com>
2023-11-15 12:03:21 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Home from "./HomePage"
+import About from "./AboutPage"
+import CartPage from "./CartPage"
+import Products from "./ProductsPage"
+import SingleProduct from "./SingleProductPage"
+import Cart from "./CartPage"
+import Error from "./ErrorPage"
+import Checkout from "./CheckoutPage"
+import PrivateRoute from "./PrivateRoute"
+import AuthWrapper from "./AuthWrapper"
+
+export {
+    Home,
+    Products,
+    About,
+    CartPage,
+    SingleProduct,
+    Cart,
+    Error,
+    Checkout,
+    PrivateRoute,
+    AuthWrapper
+}
